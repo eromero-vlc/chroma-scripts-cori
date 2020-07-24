28b04ea10f49765566f7e938d5563ff8d1e94a48
Eloy Romero <eloy@cs.wm.edu>
2020-07-24 08:38:04 -0700

    Update heuristic to remove launched mark

--- create_prop_check.sh
+++ create_prop_check.sh
@@ -2,6 +2,18 @@
 
 runpath="$PWD/cl21_32_64_b6p3_m0p2350_m0p2050"
 
-for i in `ls $runpath/run_prop_*/*.launched` ; do
-	squeue -j `cat $i` &> /dev/null || grep -q 'CHROMA: ran successfully' ${i%.sh.launched}.out || rm -f $i
+ok=0
+fail=0
+for i in $runpath/run_prop_*/*.launched ; do
+	[ -f ${i%.sh.launched}.verified ] && continue
+	squeue -j `cat $i` &> /dev/null && continue
+	if grep -q 'CHROMA: ran successfully' ${i%.sh.launched}.out &> /dev/null ; then
+		touch ${i%.sh.launched}.verified
+		ok="$(( ok+1 ))"
+	else
+		echo Removing $i
+		fail="$(( fail+1 ))"
+		rm -f $i
+	fi
 done
+echo OK: $ok  Fails: $fail
