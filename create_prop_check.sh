ac69f480a1696255e8d52b663513f1545a9ce394
Eloy Romero <eloy@cs.wm.edu>
2020-08-20 09:39:45 -0700

    Check for nans.

--- create_prop_check.sh
+++ create_prop_check.sh
@@ -4,10 +4,15 @@ runpath="$PWD/cl21_32_64_b6p3_m0p2350_m0p2050"
 
 ok=0
 fail=0
+nan=0
 for i in $runpath/run_prop_*/*.launched ; do
 	[ -f ${i%.sh.launched}.verified ] && continue
 	squeue -j `cat $i` &> /dev/null && continue
-	if grep -q 'CHROMA: ran successfully' ${i%.sh.launched}.out &> /dev/null ; then
+	if grep -q 'nan' ${i%.sh.launched}.out &> /dev/null ; then
+		echo Removing $i
+		nan="$(( nan+1 ))"
+		rm -f $i
+	elif grep -q 'CHROMA: ran successfully' ${i%.sh.launched}.out &> /dev/null ; then
 		touch ${i%.sh.launched}.verified
 		ok="$(( ok+1 ))"
 	else
@@ -16,4 +21,4 @@ for i in $runpath/run_prop_*/*.launched ; do
 		rm -f $i
 	fi
 done
-echo OK: $ok  Fails: $fail
+echo OK: $ok  Fails: $fail  nan: $nan
