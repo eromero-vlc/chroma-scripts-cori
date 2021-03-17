a72442089f7746412c8576e7174928940f7a92de
Eloy Romero <eloy@cs.wm.edu>
2021-03-17 06:09:32 -0700

    Add phased in checking eigs.

--- create_eigs_check.sh
+++ create_eigs_check.sh
@@ -2,7 +2,7 @@
 
 runpath="$PWD/cl21_32_64_b6p3_m0p2350_m0p2050"
 
-for i in `ls $runpath/run_eigs_*/*.launched` ; do
+for i in `ls $runpath/run_eigs{,phase}_*/*.launched` ; do
 	[ -f ${i%.bash.launched}.verified ] && continue
 	squeue -j `cat $i` &> /dev/null && continue
 	if grep -q 'FINISHED' ${i%.bash.launched}.out ; then
