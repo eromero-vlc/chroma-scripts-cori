9eea9e6b465de28095bc7d430a760e6f5b137eac
Eloy Romero Alcalde <eromero@jlab.org>
2023-06-08 11:34:56 -0400

    Add ensemble configuration

--- ensembles.sh
+++ ensembles.sh
@@ -1,85 +1,104 @@
 # This shell script is executed at the beginning of create_*.sh, launch.sh, cancel.sh and check.sh
 
-ensembles="ensemble0"
+ensembles="ensemble0 ensemble1 ensemble2 ensemble3"
 
 ensemble0() {
+	meta_ensemble 0.04 40
+}
+
+ensemble1() {
+	meta_ensemble 0.08 20
+}
+
+ensemble2() {
+	meta_ensemble 0.12 13
+}
+
+ensemble3() {
+	meta_ensemble 0.16 10
+}
+
+
+meta_ensemble() {
+	eigs_smear_rho="$1" # smearing factor
+	eigs_smear_steps="$2" # smearing steps
+	experiment="smear_rho${1}_steps${2}"
+
 	# Tasks to run
-	run_eigs="nop"
-	run_props="nop"
+	run_eigs="yes"
+	run_props="yes"
 	run_gprops="nop"
 	run_baryons="yes"
 	run_mesons="nop"
-	run_discos="yes"
-	run_redstar="nop"
+	run_discos="nop"
+	run_redstar="yes"
 
 	# Ensemble properties
-	confsprefix="cl21_32_64_b6p3_m0p2350_m0p2050-5162"
-	ensemble="cl21_32_64_b6p3_m0p2350_m0p2050"
-	confsname="cl21_32_64_b6p3_m0p2350_m0p2050"
-	tag="cl21_32_64_b6p3_m0p2350_m0p2050-5162"
-	confs="`seq 11000 10 13990`"
-	s_size=32 # lattice spatial size
-	t_size=64 # lattice temporal size
+	confsprefix="cl21_64_192_b6p7_m0p1830_m0p1650" # ensemble path after $confspath
+	ensemble="cl21_64_192_b6p7_m0p1830_m0p1650" # ensemble name for chroma_python
+	confsname="cl21_64_192_b6p7_m0p1830_m0p1650" # ensemble name prefixing files
+	tag="cl21_64_192_b6p7_m0p1830_m0p1650_${experiment}" # directory name for storing jobs and xmls
+	confs="`seq 1000 10 1400`"   # configuration numbers to work with
+	s_size=64 # lattice spatial size
+	t_size=192 # lattice temporal size
 
 	# configuration filename
 	lime_file_name() { echo "${confspath}/${confsprefix}/cfgs/${confsname}_cfg_${cfg}.lime"; }
-	lime_transfer_from_jlab="nop"
+	lime_transfer_from_jlab="yes"
 
 	# Colorvecs options
 	max_nvec=128  # colorvecs to compute
-	nvec=64  # colorvecs to use
-	eigs_smear_rho=0.08 # smearing factor
-	eigs_smear_steps=10 # smearing steps
+	nvec=128  # colorvecs to use
 	# colorvec filename
-	colorvec_file_name() { echo "${confspath}/${confsprefix}/eigs_mod/${confsname}.3d.eigs.n${max_nvec}.mod${cfg}"; }
-	eigs_slurm_nodes=2
-	eigs_chroma_geometry="1 2 2 4"
-	eigs_chroma_minutes=600
-	eigs_transfer_back="yes"
+	colorvec_file_name() { echo "${confspath}/${confsprefix}/eigs_mod/${confsname}_${experiment}.3d.eigs.n${max_nvec}.mod${cfg}"; }
+	eigs_slurm_nodes=1
+	eigs_chroma_geometry="1 1 1 8"
+	eigs_chroma_minutes=120
+	eigs_transfer_back="nop"
 	eigs_delete_after_transfer_back="nop"
 	eigs_transfer_from_jlab="nop"
 
 	# Props options
-	prop_t_sources="`seq 0 63`"
-	prop_t_fwd=64
-	prop_t_back=0
-	prop_nvec=96
+	prop_t_sources="0 48 96 144"
+	prop_t_fwd=16
+	prop_t_back=16
+	prop_nvec=128
 	prop_zphases="0.00"
-	prop_mass="-0.2350"
-	prop_clov="1.20536588031793"
+	prop_mass="-0.1830"
+	prop_clov="1.14272664055312"
 	prop_mass_label="U${prop_mass}"
-	prop_slurm_nodes=2
-	prop_chroma_geometry="1 2 2 4"
-	prop_chroma_minutes=600
+	prop_slurm_nodes=4
+	prop_chroma_geometry="1 2 4 4"
+	prop_chroma_minutes=120
 	# propagator filename
 	prop_file_name() {
 		if [ ${zphase} == 0.00 ]; then
-			echo "${confspath}/${confsprefix}/prop_db/${confsname}.prop.n${prop_nvec}.light.t0_${t_source}.sdb${cfg}"
+			echo "${confspath}/${confsprefix}/prop_db/${confsname}_${experiment}.prop.n${prop_nvec}.light.t0_${t_source}.sdb${cfg}"
 		else
-			echo "${confspath}/${confsprefix}/phased/prop_db/d001_${zphase}/${cfg}/${confsname}.phased_${zphase}.prop.n${prop_nvec}.light.t0_${t_source}.sdb${cfg}"
+			echo "${confspath}/${confsprefix}/phased/prop_db/d001_${zphase}/${cfg}/${confsname}_${experiment}.phased_${zphase}.prop.n${prop_nvec}.light.t0_${t_source}.sdb${cfg}"
 		fi
 	}
-	prop_transfer_back="yes"
-	prop_delete_after_transfer_back="yes"
-	prop_transfer_from_jlab="yes"
+	prop_transfer_back="nop"
+	prop_delete_after_transfer_back="nop"
+	prop_transfer_from_jlab="nop"
 
 	# Genprops options
-	gprop_t_sources="0 16 32 48"
+	gprop_t_sources="0 32 64 96"
 	gprop_t_seps="4 6 8 10 12 14"
 	gprop_zphases="0.00 2.00"
 	gprop_nvec=$nvec
-	gprop_slurm_nodes=2
-	gprop_chroma_geometry="1 2 2 4"
-	gprop_chroma_minutes=600
+	gprop_slurm_nodes=8
+	gprop_chroma_geometry="1 4 4 4"
+	gprop_chroma_minutes=120
 	gprop_file_name() {
 		if [ $zphase == 0.00 ]; then
-			echo "${confspath}/${confsprefix}/unsmeared_meson_dbs/t0_${t_source}/unsmeared_meson.n${gprop_nvec}.${t_source}.tsnk_${t_seps_commas}.Gamma_gt_g5gz_g5gx_g5gy_g5gt_gxgy_gxgz_gxgt_gygz_gygt_gzgt.absDisp000-008.qXYZ_0,0,0.sdb${cfg}"
+			echo "${confspath}/${confsprefix}/unsmeared_meson_dbs/t0_${t_source}/unsmeared_meson_${experiment}.n${gprop_nvec}.${t_source}.tsnk_${t_seps_commas}.Gamma_gt_g5gz_g5gx_g5gy_g5gt_gxgy_gxgz_gxgt_gygz_gygt_gzgt.absDisp000-008.qXYZ_0,0,0.sdb${cfg}"
 		else
-			echo "${confspath}/${confsprefix}/phased/unsmeared_meson_dbs/d001_${zphase}/t0_${t_source}/unsmeared_meson.phased_d001_${zphase}.n${gprop_nvec}.${t_source}.tsnk_${t_seps_commas}.Gamma_gt_g5gz_g5gx_g5gy_g5gt_gxgy_gxgz_gxgt_gygz_gygt_gzgt.absDisp000-008.qXYZ_0,0,0.sdb${cfg}"
+			echo "${confspath}/${confsprefix}/phased/unsmeared_meson_dbs/d001_${zphase}/t0_${t_source}/unsmeared_meson_${experiment}.phased_d001_${zphase}.n${gprop_nvec}.${t_source}.tsnk_${t_seps_commas}.Gamma_gt_g5gz_g5gx_g5gy_g5gt_gxgy_gxgz_gxgt_gygz_gygt_gzgt.absDisp000-008.qXYZ_0,0,0.sdb${cfg}"
 		fi
 	}
-	gprop_transfer_back="yes"
-	gprop_delete_after_transfer_back="yes"
+	gprop_transfer_back="nop"
+	gprop_delete_after_transfer_back="nop"
 	gprop_transfer_from_jlab="nop"
 
 	# Meson options
@@ -88,13 +107,13 @@ ensemble0() {
 	meson_slurm_nodes=2
 	meson_chroma_max_tslices_in_contraction="1" # as large as possible
 	meson_chroma_geometry="1 2 2 4"
-	meson_chroma_minutes=600
-	meson_chroma_parts=4 # split the time slices into this many different files
+	meson_chroma_minutes=120
+	meson_chroma_parts=8 # split the time slices into this many different files
 	meson_file_name() {
 		if [ ${zphase} == 0.00 ]; then
-			n="${confspath}/${confsprefix}/meson_db/${confsname}.n${meson_nvec}.m2_0_0.meson.colorvec.t_0_$((t_size-1)).sdb${cfg}"
+			n="${confspath}/${confsprefix}/meson_db/${confsname}_${experiment}.n${meson_nvec}.m2_0_0.meson.colorvec.t_0_$((t_size-1)).sdb${cfg}"
 		else
-			n="${confspath}/${confsprefix}/meson_db/${confsname}.n${meson_nvec}.m2_0_0.meson.colorvec.t_0_$((t_size-1)).phased_${zphase}.sdb${cfg}"
+			n="${confspath}/${confsprefix}/meson_db/${confsname}_${experiment}.n${meson_nvec}.m2_0_0.meson.colorvec.t_0_$((t_size-1)).phased_${zphase}.sdb${cfg}"
 		fi
 		if [ $meson_chroma_parts == 1 ]; then
 			echo $n
@@ -152,13 +171,13 @@ ensemble0() {
 	baryon_chroma_max_moms_in_contraction=1 # as large as possible (zero means do all momenta at once)
 	baryon_slurm_nodes=2
 	baryon_chroma_geometry="1 2 2 4"
-	baryon_chroma_minutes=600
-	baryon_chroma_parts=8 # split the time slices into this many different files
+	baryon_chroma_minutes=120
+	baryon_chroma_parts=16 # split the time slices into this many different files
 	baryon_file_name() {
 		if [ ${zphase} == 0.00 ]; then
-			n="${confspath}/${confsprefix}/baryon_db/${confsname}.n${baryon_nvec}.m2_0_0.baryon.colorvec.t_0_$((t_size-1)).sdb${cfg}"
+			n="${confspath}/${confsprefix}/baryon_db/${confsname}_${experiment}.n${baryon_nvec}.m2_0_0.baryon.colorvec.t_0_$((t_size-1)).sdb${cfg}"
 		else
-			n="${confspath}/${confsprefix}/baryon_db/${confsname}.n${baryon_nvec}.m2_0_0.baryon.colorvec.t_0_$((t_size-1)).phased_${zphase}.sdb${cfg}"
+			n="${confspath}/${confsprefix}/baryon_db/${confsname}_${experiment}.n${baryon_nvec}.m2_0_0.baryon.colorvec.t_0_$((t_size-1)).phased_${zphase}.sdb${cfg}"
 		fi
 		if [ $baryon_chroma_parts == 1 ]; then
 			echo $n
@@ -168,7 +187,7 @@ ensemble0() {
 			done
 		fi
 	}
-	baryon_transfer_back="yes"
+	baryon_transfer_back="nop"
 	baryon_delete_after_transfer_back="nop"
 	baryon_transfer_from_jlab="nop"
 	baryon_extra_xml="
@@ -216,7 +235,7 @@ ensemble0() {
 	disco_file_name() {
 		echo "${confspath}/${confsprefix}/disco/${confsname}.disco.sdb${cfg}"
 	}
-	disco_transfer_back="yes"
+	disco_transfer_back="nop"
 	disco_delete_after_transfer_back="nop"
 	disco_transfer_from_jlab="nop"
 
@@ -233,11 +252,8 @@ ensemble0() {
 	redstar_use_baryon="yes"
 	redstar_use_disco="nop"
 	redstar_irmom_momtype="\
-0 0 0  0 0 0
-0 0 1  1 0 0
-0 0 2  2 0 0
-0 0 3  3 0 0"
-	redstar_zeromom_operators="NucleonMG1g1MxD0J0S_J1o2_G1g1 NucleonMG1g1MxD2J0S_J1o2_G1g1 NucleonMG1g1MxD2J0M_J1o2_G1g1 NucleonMHg1SxD2J2M_J1o2_G1g1 NucleonMG1g1MxD2J1A_J1o2_G1g1 NucleonMHg1SxD2J1M_J1o2_G1g1 NucleonMG1g1MxD2J1M_J1o2_G1g1"
+0 0 0  0 0 0"
+	redstar_zeromom_operators="NucleonMG1g1MxD0J0S_J1o2_G1g1"
 	redstar_nonzeromom_operators="NucleonMG1g1MxD0J0S_J1o2_H1o2D4E1 NucleonMG1g1MxD1J1M_J1o2_H1o2D4E1 NucleonMG1g1MxD1J1M_J3o2_H1o2D4E1 NucleonMG1g1MxD2J0M_J1o2_H1o2D4E1 NucleonMG1g1MxD2J1A_J1o2_H1o2D4E1 NucleonMG1g1MxD2J1M_J1o2_H1o2D4E1 NucleonMG1g1MxD2J2M_J3o2_H1o2D4E1 NucleonMG1g1MxD2J2S_J3o2_H1o2D4E1 NucleonMG1g1MxD2J2S_J5o2_H1o2D4E1 NucleonMHg1SxD1J1M_J1o2_H1o2D4E1 NucleonMHg1SxD1J1M_J3o2_H1o2D4E1 NucleonMHg1SxD1J1M_J5o2_H1o2D4E1 NucleonMHg1SxD2J0M_J3o2_H1o2D4E1 NucleonMHg1SxD2J1M_J1o2_H1o2D4E1 NucleonMHg1SxD2J2M_J1o2_H1o2D4E1 NucleonMHg1SxD2J2M_J3o2_H1o2D4E1"
 	corr_file_name() {
 		echo "${confspath}/${confsprefix}/corr/${confsname}.nuc_local.n${redstar_nvec}.tsrc_${t_source}.${redstar_tag}.mom_${mom// /_}.sdb${cfg}"
@@ -249,26 +265,28 @@ ensemble0() {
 	redstar_transfer_from_jlab="nop"
 }
 
-chroma_python="$HOME/hadron/runs-eloy/chroma_python"
+chroma_python="$PWD/chroma_python"
 PYTHON=python3
 
 #
 # SLURM configuration for eigs, props, genprops, baryons and mesons
 #
 
-chromaform="$HOME/hadron/chromaform-perlmutter"
-chroma="$chromaform/install/chroma-quda-qdp-jit-double-nd4-cmake-superbblas-cuda/bin/chroma"
+chromaform="$HOME/chromaform_frontier_rocm5.4"
+chromaform="$HOME/chromaform_frontier_rocm4.5"
+#chroma="$chromaform/install/chroma-quda-qdp-jit-double-nd4-cmake-superbblas-hip/bin/chroma"
+chroma="$chromaform/install/chroma-sp-qdp-jit-double-nd4-cmake-superbblas-hip-next/bin/chroma"
+chroma="$chromaform/install/chroma-sp-quda-qdp-jit-double-nd4-cmake-superbblas-hip-next/bin/chroma"
 chroma_extra_args="-pool-max-alloc 0 -pool-max-alignment 512"
 redstar_corr_graph="$chromaform/install/redstar-hip/bin/redstar_corr_graph"
 redstar_npt="$chromaform/install/redstar-hip/bin/redstar_npt"
 
-slurm_procs_per_node=4
+slurm_procs_per_node=8
 slurm_sbatch_prologue="#!/bin/bash
-#SBATCH -A hadron_g
-#SBATCH -C gpu
-#SBATCH -q regular
+#SBATCH -A NPH122
+#SBATCH -p batch
 #SBATCH --gpu-bind=none
-#SBATCH --cpus-per-task=32 # number of cores per task
+#SBATCH --threads-per-core=1 --cpus-per-task=7 # number of cores per task
 #SBATCH --ntasks-per-node=$slurm_procs_per_node # number of tasks per node
 #SBATCH --gpus-per-task=1"
 
@@ -276,7 +294,7 @@ slurm_script_prologue="
 . $chromaform/env.sh
 . $chromaform/env_extra.sh
 export OPENBLAS_NUM_THREADS=1
-export OMP_NUM_THREADS=32
+export OMP_NUM_THREADS=7
 export SLURM_CPU_BIND=\"cores\"
 "
 
@@ -318,15 +336,15 @@ export ROCR_VISIBLE_DEVICES=\"\$MY_JOB_INDEX\"
 #
 
 max_jobs=20 # maximum jobs to be launched
-max_hours=5 # maximum hours for a single job
+max_hours=2 # maximum hours for a single job
 
 #
 # Path options
 #
 # NOTE: we try to recreate locally the directory structure at jlab; please give consistent paths
 
-confspath="$SCRATCH/b6p3"
-this_ep="6bdc7956-fc0f-4ad2-989c-7aa5ee643a79:${SCRATCH}/b6p3/"  # perlmutter
+confspath="$HOME/scratch"
+this_ep="ef1a9560-7ca1-11e5-992c-22000b96db58:scratch/"  # frontier
 jlab_ep="a2f9c453-2bb6-4336-919d-f195efcf327b:~/qcd/cache/isoClover/" # jlab#gw2
 jlab_local="/cache/isoClover"
 jlab_tape_registry="/mss/lattice/isoClover"
